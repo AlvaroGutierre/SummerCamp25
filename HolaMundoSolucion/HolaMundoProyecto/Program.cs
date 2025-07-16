b7c820f7a52390579abb7b27685ddb78714b5934
Alvaro Gutierre <lanchoalvaro2001@gmail.com>
2025-07-16 11:36:32 +0200

    Primer Hola Mundo

---
+++ HolaMundoSolucion/HolaMundoProyecto/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("-------------- ");
+
+        
+          Console.WriteLine("       z$$$$$. $$");
+          Console.WriteLine("      $$$$$$$$$$$");
+          Console.WriteLine("     $$$$$$**$$$$                eeeer");
+          Console.WriteLine("    $$$$$%   '$$$                $$$$F");
+          Console.WriteLine(@"   4$$$$P     *$$                $$$$F");
+          Console.WriteLine(@"   $$$$$      '$$    .ee.        $$$F ");
+          Console.WriteLine(@"   $$$$$       ""   .$$$$$$b      $$$F 4$$$$$$ ");
+          Console.WriteLine(@"   4$$$$F          4$$$""$$$$     $$$F '*$$$$*   $$$$$$");
+          Console.WriteLine(@"   4$$$$F         .$$$F  ^$$$b   $$$F  J$$$   $$$$  ^$$$.");
+          Console.WriteLine(@"   4$$$$F         d$$$    $$$$   $$$F J$$P   .$$$F   $$$$");
+          Console.WriteLine(@"   4$$$$F         $$$$    3$$$F  $$$FJ$$P    4$$$   $$$$");
+          Console.WriteLine(@"   4$$$$F        4$$$$    4$$$$  $$$$$$$r    $$$$$$$$$$$$");
+          Console.WriteLine(@"   4$$$$$        4$$$$    4$$$$  $$$$$$$$    $$$$********");
+          Console.WriteLine(@"    $$$$$        4$$$$    4$$$F  $$$F4$$$b   *$$$r");
+          Console.WriteLine(@"    3$$$$F       d$$$$    $$$$   $$$   *$$$   $$$       .");
+          Console.WriteLine(@"     $$$$$.     d$$$$$.   $$$$   $$$F  $$$$.  $$$$     z$P");
+          Console.WriteLine(@"      $$$$$e. .d$$$$$$    $$$$   J$$$L  '$$$$ '$$$b. .d$$");
+          Console.WriteLine(@"       *$$$$$$$$$  ^$$$be$$$     $$$$$$$  $$$$  $$$$$$$$");
+          Console.WriteLine(@"        ^*$$$$P       $$$$       $$$$$$$  $$$$   $$$$$");
+         
+    }
+}
